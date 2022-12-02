b94d5722533e93630508839bc74c28582aa82aea
Ronald <rvpostha15@yahoo.com>
2022-12-02 17:28:04 -0500

    commit tree c004ee884943b7447187baed342c7539a0a30baf

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Ron is the champion"
+document.body.append(newHeader);
+
+
+
