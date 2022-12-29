e5b8b1a722399b3ee15949926dd787708c53bcf3
Ronald <rvpostha15@yahoo.com>
2022-12-28 19:31:32 -0500

    lab solution

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let myMain = document.querySelector('#main');
+myMain.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Ron Posthauer is the champion';
